2c0efbf5554bdb8c746c4258fb4c68201d6be580
Nguyen Huu Lam <73806464+hlam-404error@users.noreply.github.com>
2020-11-16 22:02:27 +0700

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,15 +1,14 @@
 # https://leetcode.com/problems/two-sum/
 
 
-nums = [2,55,8,7,6]
-target = 9
-for num1 in nums:
-    for i in nums:
-        if num1 + i == target:
-            print(nums.index(num1))
-            print(nums.index(i))
-            exit()
-            
+def twosum(nums, target):
+    for num1 in nums:
+        for i in nums:
+            if num1 + i == target:
+                print(nums.index(num1))
+                
+                
+print(twosum([2,7,11,15],9))
             
 
 
