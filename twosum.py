1b03224e07e860a39aed94ecff71b86e42d91cd2
Nguyen Huu Lam <73806464+hlam-404error@users.noreply.github.com>
2020-11-16 11:25:36 +0700

    Add files via upload

---
+++ twosum.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/two-sum/
+
+
+nums = [2,55,8,7,6]
+target = 9
+for num1 in nums:
+    for i in nums:
+        if num1 + i == target:
+            print(nums.index(num1))
+            print(nums.index(i))
+            exit()
+            
+            
+
+
+
+
+
+"""
+[4, 5, 6, 7]
+8
+"""
