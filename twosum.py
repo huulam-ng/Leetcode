d1cf9bc8102e757d4d4b5b632054366b000884e9
Nguyen Huu Lam <73806464+hlam-404error@users.noreply.github.com>
2020-11-18 12:01:35 +0700

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -3,7 +3,7 @@
 
 def twosum(nums, target):
    for i in range (len(nums)):
-            for j in range(i + 1, len(nums)):
+      for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
