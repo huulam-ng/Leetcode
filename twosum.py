630880c188e7209ea6f73fd5706dd54f066432fc
Nguyen Huu Lam <73806464+hlam-404error@users.noreply.github.com>
2020-11-18 11:20:18 +0700

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -3,12 +3,9 @@
 
 def twosum(nums, target):
     for num1 in nums:
-        for i in nums:
+        for i in nums:    
             if num1 + i == target:
-                print(nums.index(num1))
-                
-                
-print(twosum([2,7,11,15],9))
+                return [nums.index(num1), nums.index(i)]
             
 
 
