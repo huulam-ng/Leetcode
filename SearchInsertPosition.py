a861082169ae1807324cd543b7f0666b3711e61c
huulam <huulam.nhl@gmail.com>
2021-06-22 17:00:46 +0700

    22/6 2

--- SearchInsertPosition.py
+++ SearchInsertPosition.py
@@ -1,10 +1,11 @@
-def searchinsertposition(nums, target: int):
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
         if target in nums:
             return nums.index(target)
         else:
-            vitri = 0
+            vitri = 0 
             for i in nums:
-                if i < target:
-                    vitri = vitri + 1
-            return vitri     
-print(searchinsertposition([1,2,4,5],3))
+                if (i < target):
+                    vitri += 1
+            return vitri
+        
