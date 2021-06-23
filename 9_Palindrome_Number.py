3ccc766d039aa308a5408e6f7733cef9ab17c260
huulam <huulam.nhl@gmail.com>
2021-06-23 23:47:57 +0700

    New

---
+++ 9_Palindrome_Number.py
@@ -0,0 +1,9 @@
+def palindrome(num: str()):
+    reverse_num = str(num)[::-1] 
+    if reverse_num == str(num):
+        return True
+    elif reverse_num != str(num):
+        return False 
+
+
+
--- mergetwosortedlists.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/merge-two-sorted-lists/
-
-def mergetwosortedlists(l1 = [], l2 = []):
-    array = l1 + l2
-    sapxep = sorted(array)
-    return sapxep
-print(mergetwosortedlists([1,3,2,4],[4,4,2,4]))
-        
-*INCOMPLETE
