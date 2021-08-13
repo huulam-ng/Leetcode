82a0ceb28d25c9d886b9e28635917315b5c22494
huulam <huulam.nhl@gmail.com>
2021-08-13 22:00:57 +0700

    13/8

---
+++ _27_removeElement.py
@@ -0,0 +1,12 @@
+def del_element(nums, val):
+    for i in range(0,len(nums)):
+        if nums[i] == val:
+            nums[i] = nums[i+1]
+    nums[len(nums)-1] = 0
+
+
+nums = [1,2,3,4]
+val = 2
+del_element(nums, val)
+
+print("done")
