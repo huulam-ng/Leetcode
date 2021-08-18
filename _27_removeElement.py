94ed05040e7f935318b3d1e5bc7269e9459f2bf2
huulam <huulam.nhl@gmail.com>
2021-08-18 22:59:20 +0700

    18/8

--- _27_removeElement.py
+++ _27_removeElement.py
@@ -1,13 +1,9 @@
-
-def del_element(nums, val):
-    for i in range(0,len(nums)):
-        if nums[i] == val:
-            nums[i] = nums[i+1]
-    nums[len(nums)-1] = 0
-
-
-nums = [1,2,3,4]
-val = 2
-del_element(nums, val)
-
-print("done")
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        k = 0
+        for i in range(0,n):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k+=1
+        return k
