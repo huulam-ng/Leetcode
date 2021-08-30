39e7924c6cc64369b2f525c6d8332c823bbf3562
huulam <huulam.nhl@gmail.com>
2021-08-30 21:26:22 +0700

    30/8

---
+++ _989_Add_to_Array_Form_of_Integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        result = ''
+        a = []
+        for i in range(0, len(num)):
+            result += str(num[i])
+        last_result = int(result) + int(k)
+        return list(''.join(str(last_result)))
