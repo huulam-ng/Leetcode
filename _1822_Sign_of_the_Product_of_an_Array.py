9bf354fdb0b9fb8ed6c06a036debe1e1f904dad7
huulam <huulam.nhl@gmail.com>
2021-09-10 00:08:05 +0700

    9/9

---
+++ _1822_Sign_of_the_Product_of_an_Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        result = 1
+        for i in nums:
+            result *= i
+        if result > 0: return 1
+        elif result == 0: return 0
+        else: return -1
